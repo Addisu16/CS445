0bc6f7c8b72b839b14fc0f11781042a4e2da2db0
Addisu16 <71025422+Addisu16@users.noreply.github.com>
2023-05-19 01:46:30 -0500

    update

---
+++ 0518-Fetch-starter/0518-Fetch-starter/server/model/products.js
@@ -0,0 +1,41 @@
+let products = [{id:1, title: 'DELL', price: 99, description: 'good'},
+                {id:2, title: 'HP', price: 299, description: 'used'},
+                {id:3, title: 'Acer', price: 399, description: 'bad'}];
+let idCounter = 4;
+
+module.exports = class Product {
+
+    constructor(id, title, price, description){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    save(){
+        this.id = idCounter++;
+        products.push(this);
+        return this;
+    }
+
+    static getById(id){
+        return products.find(prod => prod.id == id);
+    }
+
+    static getAll(){
+        return products;
+    }
+
+    static deleteById(id){
+        const index = products.findIndex(p => p.id == id);
+        const deleteProd = products[index];
+        products.splice(index, 1);
+        return deleteProd;
+    }
+
+    edit(){
+        const index = products.findIndex(p => p.id == this.id);
+        products[index] = this;
+        return this;
+    }
+}
