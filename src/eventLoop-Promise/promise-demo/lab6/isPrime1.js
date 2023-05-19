c582bec9c25940000f576fecacfebbb8e946dd15
Addisu16 <71025422+Addisu16@users.noreply.github.com>
2023-05-18 20:34:34 -0500

    lab6

---
+++ src/eventLoop-Promise/promise-demo/lab6/isPrime1.js
@@ -0,0 +1,27 @@
+async function isPrimeAsync(num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+                if (num % i === 0) reject({ prime: false });
+            resolve({ prime: num > 1 });
+        }, 500);
+    });
+};
+console.log('start');
+(async () => {
+  try {
+    const result = await isPrimeAsync(7);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log('end');
+  }
+})();
+
+// Output:
+// start
+//end
+// { prime: true}
+
+    
---
+++ src/eventLoop-Promise/promise-demo/lab6/removeDuplicatesAsync2.js
@@ -0,0 +1,20 @@
+Array.prototype.removeDuplicatesAsync=async function(){
+   const newArr=await new Promise(resolve=>{
+        setTimeout(() => {
+            const newArr=[...new Set(this)];
+            resolve(newArr);
+        }, 0);
+    })
+    .catch(error=>console.log(error));
+console.log(newArr);
+
+};
+
+console.log(`start`);
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync(); 
+console.log(`end`);
+
+// Output:
+// start
+// end
+// [4, 1, 5, 7, 2, 3, 6]
